d96b75e1f56b98396c2ae105b145d1fa121d1197
Sara Rotenski <162700489+sararotenski@users.noreply.github.com>
2025-06-13 11:50:30 -0300

    Add files via upload

--- src/components/ContentImgLeft.js
+++ src/components/ContentImgLeft.js
@@ -3,9 +3,9 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
-function ContentImgLeft({imagem, textoImagem, titulo, texto}) {
+function ContentImgLeft({imagem, textoImagem, titulo, texto, mostrarBotao = false}) {
   return (
-    <Container>
+    <Container className="py-5">
         <Row>
             <Col lg='6' md='6' sm='12'>
                 <img src={ imagem } alt= { textoImagem } className='img-fluid' />
@@ -14,6 +14,7 @@ function ContentImgLeft({imagem, textoImagem, titulo, texto}) {
             <Col lg='6' md='6' sm='12'>
                 <h2>{ titulo }</h2>
                 <p>{ texto }</p>
+                {mostrarBotao && <button>Saiba Mais</button>}
             </Col>
         </Row>
       
