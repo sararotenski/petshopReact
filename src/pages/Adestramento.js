c5c5af407ac3d7a53c7cbe67e2010c850f53b54c
Sara Rotenski <162700489+sararotenski@users.noreply.github.com>
2025-06-18 10:20:38 -0300

    Add files via upload

--- src/pages/Adestramento.js
+++ src/pages/Adestramento.js
@@ -1,10 +1,77 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import banner from '../imagens/adestramento/banner-adestramento.jpg'
+import adestramento1 from '../imagens/adestramento/adestramento1.jpg'
+import adestramento2 from '../imagens/adestramento/adestramento2.jpg'
+import adestramento3 from '../imagens/adestramento/adestramento3.jpg'
+import adestramento4 from '../imagens/adestramento/adestramento4.jpg'
+import adestramento5 from '../imagens/adestramento/adestramento5.jpg'
 
-function Adestramento() {
+
+function Adestramento() { // w = width
+
+    const photos = [
+        {
+            id: 1,
+            title: 'Imagem de Adestramento',
+            url: `${ adestramento1 }` //interpolação
+        },
+        {
+            id: 2,
+            title: 'Imagem de Adestramento',
+            url: `${ adestramento2 }` //interpolação
+        },
+        {
+            id: 3,
+            title: 'Imagem de Adestramento',
+            url: `${ adestramento3 }` //interpolação
+        },
+        {
+            id: 4,
+            title: 'Imagem de Adestramento',
+            url: `${ adestramento4 }` //interpolação
+        },
+        {
+            id: 5,
+            title: 'Imagem de Adestramento',
+            url: `${ adestramento5 }` //interpolação
+        }
+    ]
+
     return (
-        <div>
-            <h1>Adestramento</h1>
-        </div>
+        <>
+        <Container fluid>
+            <Row>
+                <Col>
+                    <img src={ banner } alt='banner adestramento' className='w-100' />
+                </Col>
+            </Row>
+        </Container>
+
+        <Container>
+            <Row className='py-5'>
+                <Col>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus gravida elit non porta egestas. Proin ultrices lectus sed sollicitudin egestas. Fusce auctor dolor turpis, at porta arcu laoreet id. Nulla.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sodales ultricies vestibulum. Praesent tristique odio urna, sit amet fringilla neque tincidunt nec. Fusce eu odio ut quam pretium dictum. Proin.</p>
+                </Col>
+
+                <Col className='photos'>
+                    {
+                        photos.map( () => (
+                            <div key={photos.id}>
+                                <img src={ photos.url } alt={ photos.title } />
+                            </div>
+                        ))
+                    }
+                </Col>
+                <Col>
+                
+                </Col>
+            </Row>
+        </Container>
+        </>
     )
 }
 
